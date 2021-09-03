f76ff7a209c46187cea1cdd54eb15730f7c769c6
Roushan Raj <roushanrj776@gmail.com>
2021-09-03 12:40:12 +0530

    UI fixes

--- src/components/pie-chart-component/PieChart.js
+++ src/components/pie-chart-component/PieChart.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 import React from "react";
 import { PieChart, Pie, Cell, Tooltip, Legend, LabelList } from 'recharts';
-
+import './PieChart.css' 
 
 export class PieChartComponent extends React.Component {
     state = {
@@ -18,24 +18,34 @@ export class PieChartComponent extends React.Component {
     }
     COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF'];
 
-
-    render() {
-        return (
-            <div class="center">
-                <PieChart  width={1030} height={400}>
-                    <Pie data={this.state.results} color="#000000" dataKey="expense" nameKey="aspect" cx="50%" cy="50%" outerRadius={160} fill="#8884d8" >
-
-                        {
-                            this.state.results.map((entry, index) => <Cell key={`cell-${index}`} fill={this.COLORS[index % this.COLORS.length]} />)
-                        }
-                        <LabelList dataKey="expense" position="top" />
-                    </Pie>
-                    
-                    <Tooltip  />
-                    <Legend align="center" />
-                </PieChart>
-                <h5 align="left">Note! Figures in Thousand INR.</h5>
-            </div>
-        )
+    CustomTooltip = ({ active, payload, label }) => {
+        if (active) {
+            return (
+                <div className="custom-tooltip" style={{ backgroundColor: '#ffff', padding: '5px', border: '1px solid #cccc' }}>
+                    <p>{`Expenditure on ${payload[0].name} : ${payload[0].value}K`}</p>
+                    <p>{'percentage: '+(`${payload[0].value}`*100/294).toFixed(2)+'%'}</p>
+                </div>
+            );
+        }     return null;
     };
-}
+
+        render() {
+            return (
+                <div class="center">
+                    <PieChart width={1030} height={400}>
+                        <Pie data={this.state.results} color="#000000" dataKey="expense" nameKey="aspect" cx="50%" cy="50%" outerRadius={160} fill="#8884d8" >
+
+                            {
+                                this.state.results.map((entry, index) => <Cell key={`cell-${index}`} fill={this.COLORS[index % this.COLORS.length]} />)
+                            }
+                            <LabelList dataKey="expense" position="top" />
+                        </Pie>
+
+                        <Tooltip content={<this.CustomTooltip />}/>
+                        <Legend align="center" />
+                    </PieChart >
+                    <h5 align="left">Note! Figures in Thousand INR.</h5>
+                </div>
+            )
+        };
+    }
